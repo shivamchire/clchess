04d3ace29c556613203ee10e583a39079e16dd5d
Shivam Hire <shivamhire123123@gmail.com>
2018-09-26 23:52:45 +0530

    added .gitignore makefile and comments in board_pieces.c

--- board_pieces.c
+++ board_pieces.c
@@ -1,6 +1,15 @@
 #include "board_pieces.h"
 #include <string.h>
 
+/*
+ * capital = white, small = black
+ * R, r = Rook
+ * N, n = Knight
+ * B, b = Bishop
+ * Q, q = Queen
+ * K, k = King
+ * P, p = pawn
+ */
 char board[8][9] = {
 	{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
 	{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
---
+++ makefile
@@ -0,0 +1,2 @@
+main: ui.c board_pieces.c main.c
+	gcc ui.c board_pieces.c main.c -o main
